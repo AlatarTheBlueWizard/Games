f89bad8a8c7c644d0c015c140ea156085454cf6e
Ryan Gale <gal17027@byui.edu>
2020-07-25 19:48:44 -0400

    Added abstract class for id and column types.

---
+++ Games.Domain/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Games.Domain
+{
+
+    // Have the entity class be an abstract class that will be extended.
+    // It is abstract because it has characteristics that all domain 
+    // classes will contain.
+
+    public abstract class Entity
+    {
+        //Set your ID as a primary key
+        [Key]
+
+        // all domain classes contain an id of type integer
+        public Guid Id { get; set; }
+    }
+}
---
+++ Games.Domain/Games/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Games.Domain.Games
+{
+    //Have all domain classes extend the entity class.
+    //Entity class is abstract and contains characteristics that all
+    //domain models have or will inherit.
+
+    public class Game : Entity
+    {
+        // Define constructor for creating and updating from db
+        // converts a view model to a domain model in services
+        // to use as an argument to the repo to create new data.
+        public Game(string title, string genre, int releaseYear, string company)
+        {
+            Title = title;
+            Genre = genre;
+            ReleaseYear = releaseYear;
+            Company = company;
+        }
+
+        // Games table will have a column with an id (inherited from
+        // the entity class) with a type of integer. The title, genre,
+        // and company column will have a type of string. Finally the
+        // release year will be a column with a type of integer.
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Company { get; set; }
+    }
+}
