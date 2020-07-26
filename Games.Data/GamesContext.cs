a7d22378fe76838dfc55f9158ecead2c3a21c8ea
Ryan Gale <gal17027@byui.edu>
2020-07-25 20:08:52 -0400

    Edited conenctions in JSON and created context class.

---
+++ Games.Data/GamesContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//Import EntityFrameworkCore that will be used to have the 
+//context class inherit the DbContext class. Using a directive.
+using Microsoft.EntityFrameworkCore;
+//Import Games.Domain when using the Domain classes
+using Games.Domain.Games;
+
+namespace Games.Data
+{
+    public class GamesContext : DbContext
+    {
+        public GamesContext(DbContextOptions options) : base(options) { }
+        public DbSet<Game> Games { get; set; }
+    }
+}
